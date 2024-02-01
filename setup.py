ffab9e13bc69ec52b248e01dc466dcb1a65a2642
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-01 18:42:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,10 +18,17 @@ def download_and_extract(url, destination):
 # Set your preferred directory
 download_dir = './'
 
+# Download and extract Chrome
+chrome_url = 'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chrome-linux64.zip'
+chrome_destination = os.path.join(download_dir, 'chrome')
+download_and_extract(chrome_url, chrome_destination)
+
 # Download and extract ChromeDriver
 chromedriver_url = 'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chromedriver-linux64.zip'
 chromedriver_destination = os.path.join(download_dir, 'chromedriver')
 download_and_extract(chromedriver_url, chromedriver_destination)
 
 chromedriver_destination = os.path.abspath(chromedriver_destination)
-print(f'Chromedriver Path: {chromedriver_destination}')
+chrome_destination = os.path.abspath(chrome_destination)
+print(f'Chromedriver Path: {chromedriver_destination}\nChrome Path: {chrome_destination})
+
