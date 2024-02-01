470e8e5a909c411db8e7f817568e06747447a8e5
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-01 21:14:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,35 +5,32 @@ from setup import chrome_destination
 
 app = Flask(__name__)
 
-# Configure Chrome options
+# Configure Chrome options outside the function
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--enable-logging')
+chrome_options.add_argument('--v=1')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-    
+
 # Set the paths for Chrome and Chromedriver
 chrome_options.binary_location = chrome_destination
-    
+
+# Create Chrome webdriver instance
+driver = webdriver.Chrome(options=chrome_options)
 
 def render_page(url):
-    # Create Chrome webdriver instance
-    driver = webdriver.Chrome(options=chrome_options)
-    
-    driver.get(url)
-    rendered_page = driver.page_source
-    
-    driver.quit()
-    
-    return rendered_page
-
-@app.route('/<path:url>')
-def proxy(url):
     try:
-        rendered_page = render_page(url)
+        driver.get(url)
+        rendered_page = driver.page_source
         return Response(rendered_page, content_type='text/html')
     except Exception as e:
         return str(e)
+
+@app.route('/<path:url>')
+def proxy(url):
+    return render_page(url)
 
 if __name__ == '__main__':
     app.run(debug=True)
