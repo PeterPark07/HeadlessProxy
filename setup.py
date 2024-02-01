f456e16d8f3c2a14fdf6ffa7fabf6341cc855459
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-01 20:57:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ import requests
 from pathlib import Path
 
 def download_and_extract(url, destination):
-    response = requests.get(url)
     zip_file = Path(destination).with_suffix('.zip')
     
     with open(zip_file, 'wb') as f:
-        f.write(response.content)
+        f.write(requests.get(url).content)
 
     with zipfile.ZipFile(zip_file, 'r') as zip_ref:
         zip_ref.extractall(destination)
@@ -22,7 +21,7 @@ download_dir = './'
 chrome_url = 'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chrome-linux64.zip'
 chrome_destination = os.path.join(download_dir, 'chrome-linux64')
 download_and_extract(chrome_url, chrome_destination)
-chrome_destination = os.path.join(chrome_destination, 'chrome-linux64/chrome')
 
-chrome_destination = os.path.abspath(chrome_destination)
+# Set Chrome executable path
+chrome_destination = os.path.abspath(os.path.join(chrome_destination, 'chrome-linux64/chrome'))
 os.chmod(chrome_destination, 0o755)
