77965f77d59d1c37632ac533940c203b33d6e5e7
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-01 17:49:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,24 +16,11 @@ def download_and_extract(url, destination):
     os.remove(zip_file)
 
 # Set your preferred directory
-download_dir = '/path/to/download'
-
-# Download and extract Chrome
-chrome_url = 'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chrome-linux64.zip'
-chrome_destination = os.path.join(download_dir, 'chrome')
-download_and_extract(chrome_url, chrome_destination)
+download_dir = './'
 
 # Download and extract ChromeDriver
 chromedriver_url = 'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chromedriver-linux64.zip'
 chromedriver_destination = os.path.join(download_dir, 'chromedriver')
 download_and_extract(chromedriver_url, chromedriver_destination)
 
-# Download and extract Chrome Headless Shell
-chrome_headless_url = 'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chrome-headless-shell-linux64.zip'
-chrome_headless_destination = os.path.join(download_dir, 'chrome-headless-shell')
-download_and_extract(chrome_headless_url, chrome_headless_destination)
-
-# Print the paths to the extracted files
-print(f'Chrome: {chrome_destination}')
 print(f'ChromeDriver: {chromedriver_destination}')
-print(f'Chrome Headless Shell: {chrome_headless_destination}')
